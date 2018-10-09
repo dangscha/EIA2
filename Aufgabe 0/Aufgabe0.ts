40634d887cc877901326f72b3069697ca80ba51d
Jan-Laptop <Jan-Laptop@DESKTOP-3BB5I02.eduroam.hs-furtwangen.de>
2018-10-09 13:58:04 +0200

    Typ geändert (any to HTMLElement)

--- Aufgabe 0/Aufgabe0.ts
+++ Aufgabe 0/Aufgabe0.ts
@@ -7,17 +7,17 @@ Datum: 04.10.2018
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 **/
 
-namespace Aufgabe0{
-    
+namespace Aufgabe0 {
 
-    function box(){
-        var name = prompt ("Wie ist dein Name");
-        var node:any= document.getElementById("content");
+
+    function box() {
+        var name = prompt("Wie ist dein Name");
+        var node: HTMLElement = document.getElementById("content");
         node.innerHTML += "Hey ";
         node.innerHTML += name;
         node.innerHTML += " ,wie geht es dir";
-        console.log("Hey",name,",wie geht es dir")
-       }
-        
-document.addEventListener('DOMContentLoaded',box);
+        console.log("Hey", name, ",wie geht es dir")
+    }
+
+    document.addEventListener('DOMContentLoaded', box);
 }
