9ea9e65a8cf33173e6f81a1332791e7aaaa1b0a5
Jan-Laptop <Jan-Laptop@DESKTOP-3BB5I02>
2018-11-08 16:02:50 +0100

    as

--- Aufgabe 3/Aufgabe3.ts
+++ Aufgabe 3/Aufgabe3.ts
@@ -8,499 +8,41 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 **/
 
 namespace Aufgabe31 {
-  
-        interface Cards {
-            color: string;
-            typ: string;
-        }
-        let cardblue1 = {
-            color: "#0000ff",
-            typ: "0",
+    document.addEventListener("DOMContentLoaded", uno);
+
+    interface Card {
+        color: string;
+        typ: string;
+    }
+
+    let AllCards: Card[] = [{ color: "#0000ff", typ: "0" }, { color: "#0000ff", typ: "1" }, { color: "#0000ff", typ: "1" }, { color: "#0000ff", typ: "2" }, { color: "#0000ff", typ: "2" }, { color: "#0000ff", typ: "3" }, { color: "#0000ff", typ: "3" }, { color: "#0000ff", typ: "4" }, { color: "#0000ff", typ: "4" }, { color: "#0000ff", typ: "5" }, { color: "#0000ff", typ: "5" }, { color: "#0000ff", typ: "6" }, { color: "#0000ff", typ: "6" }, { color: "#0000ff", typ: "7" }, { color: "#0000ff", typ: "7" }, { color: "#0000ff", typ: "8" }, { color: "#0000ff", typ: "8" }, { color: "#0000ff", typ: "9" }, { color: "#0000ff", typ: "9" }, { color: "#0000ff", typ: "Reverse" }, { color: "#0000ff", typ: "Reverse" }, { color: "#0000ff", typ: "Plus 2" }, { color: "#0000ff", typ: "Plus 2" }, { color: "#0000ff", typ: "Skip" }, { color: "#0000ff", typ: "Skip" }, { color: "#ff0000", typ: "0" }, { color: "#ff0000", typ: "1" }, { color: "#ff0000", typ: "1" }, { color: "#ff0000", typ: "2" }, { color: "#ff0000", typ: "2" }, { color: "#ff0000", typ: "3" }, { color: "#ff0000", typ: "3" }, { color: "#ff0000", typ: "4" }, { color: "#ff0000", typ: "4" }, { color: "#ff0000", typ: "5" }, { color: "#ff0000", typ: "5" }, { color: "#ff0000", typ: "6" }, { color: "#ff0000", typ: "6" }, { color: "#ff0000", typ: "7" }, { color: "#ff0000", typ: "7" }, { color: "#ff0000", typ: "8" }, { color: "#ff0000", typ: "8" }, { color: "#ff0000", typ: "9" }, { color: "#ff0000", typ: "9" }, { color: "#ff0000", typ: "Reverse" }, { color: "#ff0000", typ: "Reverse" }, { color: "#ff0000", typ: "Plus 2" }, { color: "#ff0000", typ: "Plus 2" }, { color: "#ff0000", typ: "Skip" }, { color: "#ff0000", typ: "Skip" }, { color: "#ffff00", typ: "0" }, { color: "#ffff00", typ: "1" }, { color: "#ffff00", typ: "1" }, { color: "#ffff00", typ: "2" }, { color: "#ffff00", typ: "2" }, { color: "#ffff00", typ: "3" }, { color: "#ffff00", typ: "3" }, { color: "#ffff00", typ: "4" }, { color: "#ffff00", typ: "4" }, { color: "#ffff00", typ: "5" }, { color: "#ffff00", typ: "5" }, { color: "#ffff00", typ: "6" }, { color: "#ffff00", typ: "6" }, { color: "#ffff00", typ: "7" }, { color: "#ffff00", typ: "7" }, { color: "#ffff00", typ: "8" }, { color: "#ffff00", typ: "8" }, { color: "#ffff00", typ: "9" }, { color: "#ffff00", typ: "9" }, { color: "#ffff00", typ: "Reverse" }, { color: "#ffff00", typ: "Reverse" }, { color: "#ffff00", typ: "Plus 2" }, { color: "#ffff00", typ: "Plus 2" }, { color: "#ffff00", typ: "Skip" }, { color: "#ffff00", typ: "Skip" }, { color: "#00ff00", typ: "0" }, { color: "#00ff00", typ: "1" }, { color: "#00ff00", typ: "1" }, { color: "#00ff00", typ: "2" }, { color: "#00ff00", typ: "2" }, { color: "#00ff00", typ: "3" }, { color: "#00ff00", typ: "3" }, { color: "#00ff00", typ: "4" }, { color: "#00ff00", typ: "4" }, { color: "#00ff00", typ: "5" }, { color: "#00ff00", typ: "5" }, { color: "#00ff00", typ: "6" }, { color: "#00ff00", typ: "6" }, { color: "#00ff00", typ: "7" }, { color: "#00ff00", typ: "7" }, { color: "#00ff00", typ: "8" }, { color: "#00ff00", typ: "8" }, { color: "#00ff00", typ: "9" }, { color: "#00ff00", typ: "9" }, { color: "#00ff00", typ: "Reverse" }, { color: "#00ff00", typ: "Reverse" }, { color: "#00ff00", typ: "Plus 2" }, { color: "#00ff00", typ: "Plus 2" }, { color: "#00ff00", typ: "Skip" }, { color: "#00ff00", typ: "Skip" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Color" }, { color: "#000000", typ: "Color" }, { color: "#000000", typ: "Color" }, { color: "#000000", typ: "Color" },]
+    let HandCards: Card[] = [];
+
+    function CreateRandomNumber(x: number) {
+        return Math.floor(Math.random() * Math.floor(x))
+    }
+
+    function uno() {
+        function EventListenerSortCards() {
+            let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
+            button.addEventListener("click", SortCards)
         }
 
-        let cardblue2 = {
-            color: "#0000ff",
-            typ: "1",
+        function SortCards() {
+            HandCards.sort(function(a, b) { return a.typ - b.typ });
+            console.log(HandCards)
         }
 
-        let cardblue3 = {
-            color: "#0000ff",
-            typ: "1",
-        }
-
-
-        let cardblue4 = {
-            color: "#0000ff",
-            typ: "2",
-        }
-        let cardblue5 = {
-            color: "#0000ff",
-            typ: "2",
-        }
-
-        let cardblue6 = {
-            color: "#0000ff",
-            typ: "3",
-        }
-        let cardblue7 = {
-            color: "#0000ff",
-            typ: "3",
-        }
-
-        let cardblue8 = {
-            color: "#0000ff",
-            typ: "4",
-        }
-        let cardblue9 = {
-            color: "#0000ff",
-            typ: "4",
-        }
-
-        let cardblue10 = {
-            color: "#0000ff",
-            typ: "5",
-        }
-        let cardblue11 = {
-            color: "#0000ff",
-            typ: "5",
-        }
-        let cardblue12 = {
-            color: "#0000ff",
-            typ: "6",
-        }
-        let cardblue13 = {
-            color: "#0000ff",
-            typ: "6",
-        }
-
-        let cardblue14 = {
-            color: "#0000ff",
-            typ: "7",
-        }
-        let cardblue15 = {
-            color: "#0000ff",
-            typ: "7",
-        }
-        let cardblue16 = {
-            color: "#0000ff",
-            typ: "8",
-        }
-        let cardblue17 = {
-            color: "#0000ff",
-            typ: "8",
-        }
-        let cardblue18 = {
-            color: "#0000ff",
-            typ: "9",
-        }
-        let cardblue19 = {
-            color: "#0000ff",
-            typ: "9",
-        }
-        let cardblue20 = {
-            color: "#0000ff",
-            typ: "Reverse",
-        }
-        let cardblue21 = {
-            color: "#0000ff",
-            typ: "Reverse",
-        }
-        let cardblue22 = {
-            color: "#0000ff",
-            typ: "Plus 2",
-        }
-        let cardblue23 = {
-            color: "#0000ff",
-            typ: "Plus 2",
-        }
-
-        let cardblue24 = {
-            color: "#0000ff",
-            typ: "Skip",
-        }
-        let cardblue25 = {
-            color: "#0000ff",
-            typ: "Skip",
-        }
-        let cardred1 = {
-            color: "#ff0000",
-            typ: "0",
-        }
-
-        let cardred2 = {
-            color: "#ff0000",
-            typ: "1",
-        }
-
-        let cardred3 = {
-            color: "#ff0000",
-            typ: "1",
-        }
-
-
-        let cardred4 = {
-            color: "#ff0000",
-            typ: "2",
-        }
-        let cardred5 = {
-            color: "#ff0000",
-            typ: "2",
-        }
-
-        let cardred6 = {
-            color: "#ff0000",
-            typ: "3",
-        }
-        let cardred7 = {
-            color: "#ff0000",
-            typ: "3",
-        }
-
-        let cardred8 = {
-            color: "#ff0000",
-            typ: "4",
-        }
-        let cardred9 = {
-            color: "#ff0000",
-            typ: "4",
-        }
-
-        let cardred10 = {
-            color: "#ff0000",
-            typ: "5",
-        }
-        let cardred11 = {
-            color: "#ff0000",
-            typ: "5",
-        }
-        let cardred12 = {
-            color: "#ff0000",
-            typ: "6",
-        }
-        let cardred13 = {
-            color: "#ff0000",
-            typ: "6",
-        }
-
-        let cardred14 = {
-            color: "#ff0000",
-            typ: "7",
-        }
-        let cardred15 = {
-            color: "#ff0000",
-            typ: "7",
-        }
-        let cardred16 = {
-            color: "#ff0000",
-            typ: "8",
-        }
-        let cardred17 = {
-            color: "#ff0000",
-            typ: "8",
-        }
-        let cardred18 = {
-            color: "#ff0000",
-            typ: "9",
-        }
-        let cardred19 = {
-            color: "#ff0000",
-            typ: "9",
-        }
-        let cardred20 = {
-            color: "#ff0000",
-            typ: "Reverse",
-        }
-        let cardred21 = {
-            color: "#ff0000",
-            typ: "Reverse",
-        }
-        let cardred22 = {
-            color: "#ff0000",
-            typ: "Plus 2",
-        }
-        let cardred23 = {
-            color: "#ff0000",
-            typ: "Plus 2",
-        }
-
-        let cardred24 = {
-            color: "#ff0000",
-            typ: "Skip",
-        }
-        let cardred25 = {
-            color: "#ff0000",
-            typ: "Skip",
-        }
-        let cardyellow1 = {
-            color: "#ffff00",
-            typ: "0",
-        }
-
-        let cardyellow2 = {
-            color: "#ffff00",
-            typ: "1",
-        }
-
-        let cardyellow3 = {
-            color: "#ffff00",
-            typ: "1",
-        }
-
-
-        let cardyellow4 = {
-            color: "#ffff00",
-            typ: "2",
-        }
-        let cardyellow5 = {
-            color: "#ffff00",
-            typ: "2",
-        }
-
-        let cardyellow6 = {
-            color: "#ffff00",
-            typ: "3",
-        }
-        let cardyellow7 = {
-            color: "#ffff00",
-            typ: "3",
-        }
-
-        let cardyellow8 = {
-            color: "#ffff00",
-            typ: "4",
-        }
-        let cardyellow9 = {
-            color: "#ffff00",
-            typ: "4",
-        }
-
-        let cardyellow10 = {
-            color: "#ffff00",
-            typ: "5",
-        }
-        let cardyellow11 = {
-            color: "#ffff00",
-            typ: "5",
-        }
-        let cardyellow12 = {
-            color: "#ffff00",
-            typ: "6",
-        }
-        let cardyellow13 = {
-            color: "#ffff00",
-            typ: "6",
-        }
-
-        let cardyellow14 = {
-            color: "#ffff00",
-            typ: "7",
-        }
-        let cardyellow15 = {
-            color: "#ffff00",
-            typ: "7",
-        }
-        let cardyellow16 = {
-            color: "#ffff00",
-            typ: "8",
-        }
-        let cardyellow17 = {
-            color: "#ffff00",
-            typ: "8",
-        }
-        let cardyellow18 = {
-            color: "#ffff00",
-            typ: "9",
-        }
-        let cardyellow19 = {
-            color: "#ffff00",
-            typ: "9",
-        }
-        let cardyellow20 = {
-            color: "#ffff00",
-            typ: "Reverse",
-        }
-        let cardyellow21 = {
-            color: "#ffff00",
-            typ: "Reverse",
-        }
-        let cardyellow22 = {
-            color: "#ffff00",
-            typ: "Plus 2",
-        }
-        let cardyellow23 = {
-            color: "#ffff00",
-            typ: "Plus 2",
-        }
-
-        let cardyellow24 = {
-            color: "#ffff00",
-            typ: "Skip",
-        }
-        let cardyellow25 = {
-            color: "#ffff00",
-            typ: "Skip",
-        }
-
-        let cardgreen1 = {
-            color: "#00ff00",
-            typ: "0",
-        }
-
-        let cardgreen2 = {
-            color: "#00ff00",
-            typ: "1",
-        }
-
-        let cardgreen3 = {
-            color: "#00ff00",
-            typ: "1",
-        }
-
-
-        let cardgreen4 = {
-            color: "#00ff00",
-            typ: "2",
-        }
-        let cardgreen5 = {
-            color: "#00ff00",
-            typ: "2",
-        }
-
-        let cardgreen6 = {
-            color: "#00ff00",
-            typ: "3",
-        }
-        let cardgreen7 = {
-            color: "#00ff00",
-            typ: "3",
-        }
-
-        let cardgreen8 = {
-            color: "#00ff00",
-            typ: "4",
-        }
-        let cardgreen9 = {
-            color: "#00ff00",
-            typ: "4",
-        }
-
-        let cardgreen10 = {
-            color: "#00ff00",
-            typ: "5",
-        }
-        let cardgreen11 = {
-            color: "#00ff00",
-            typ: "5",
-        }
-        let cardgreen12 = {
-            color: "#00ff00",
-            typ: "6",
-        }
-        let cardgreen13 = {
-            color: "#00ff00",
-            typ: "6",
-        }
-
-        let cardgreen14 = {
-            color: "#00ff00",
-            typ: "7",
-        }
-        let cardgreen15 = {
-            color: "#00ff00",
-            typ: "7",
-        }
-        let cardgreen16 = {
-            color: "#00ff00",
-            typ: "8",
-        }
-        let cardgreen17 = {
-            color: "#00ff00",
-            typ: "8",
-        }
-        let cardgreen18 = {
-            color: "#00ff00",
-            typ: "9",
-        }
-        let cardgreen19 = {
-            color: "#00ff00",
-            typ: "9",
-        }
-        let cardgreen20 = {
-            color: "#00ff00",
-            typ: "Reverse",
-        }
-        let cardgreen21 = {
-            color: "#00ff00",
-            typ: "Reverse",
-        }
-        let cardgreen22 = {
-            color: "#00ff00",
-            typ: "Plus 2",
-        }
-        let cardgreen23 = {
-            color: "#00ff00",
-            typ: "Plus 2",
-        }
-
-        let cardgreen24 = {
-            color: "#00ff00",
-            typ: "Skip",
-        }
-        let cardgreen25 = {
-            color: "#00ff00",
-            typ: "Skip",
-        }
-
-        let cardblack1 = {
-            color: "#000000",
-            typ: "Plus 4",
-        }
-        let cardblack2 = {
-            color: "#000000",
-            typ: "Plus 4",
-        }
-        let cardblack3 = {
-            color: "#000000",
-            typ: "Plus 4",
-        }
-        let cardblack4 = {
-            color: "#000000",
-            typ: "Plus 4",
-        }
-        let cardblack5 = {
-            color: "#000000",
-            typ: "Color",
-        }
-        let cardblack6 = {
-            color: "#000000",
-            typ: "Color",
-        }
-        let cardblack7 = {
-            color: "#000000",
-            typ: "Color",
-        }
-        let cardblack8 = {
-            color: "#000000",
-            typ: "Color",
-        }
-
-        let AllCards: Cards[] = [cardblue1, cardblue2, cardblue3, cardblue4, cardblue5, cardblue6, cardblue7, cardblue8, cardblue9, cardblue10, cardblue11, cardblue12, cardblue13, cardblue14, cardblue15, cardblue16, cardblue17, cardblue18, cardblue19, cardblue20, cardblue21, cardblue22, cardblue23, cardblue24, cardblue25, cardred1, cardred2, cardred3, cardred4, cardred5, cardred6, cardred7, cardred8, cardred9, cardred10, cardred11, cardred12, cardred13, cardred14, cardred15, cardred16, cardred17, cardred18, cardred19, cardred20, cardred21, cardred22, cardred23, cardred24, cardred25, cardgreen1, cardgreen2, cardgreen3, cardgreen4, cardgreen5, cardgreen6, cardgreen7, cardgreen8, cardgreen9, cardgreen10, cardgreen11, cardgreen12, cardgreen13, cardgreen14, cardgreen15, cardgreen16, cardgreen17, cardgreen18, cardgreen19, cardgreen20, cardgreen21, cardgreen22, cardgreen23, cardgreen24, cardgreen25, cardyellow1, cardyellow2, cardyellow3, cardyellow4, cardyellow5, cardyellow6, cardyellow7, cardyellow8, cardyellow9, cardyellow10, cardyellow11, cardyellow12, cardyellow13, cardyellow14, cardyellow15, cardyellow16, cardyellow17, cardyellow18, cardyellow19, cardyellow20, cardyellow21, cardyellow22, cardyellow23, cardyellow24, cardyellow25, cardblack1, cardblack2, cardblack3, cardblack4, cardblack5, cardblack6, cardblack7, cardblack8]
-        let HandCards:string[]=[];
-        function random(x: number) {
-            return Math.floor(Math.random() * Math.floor(x))
-        }
-      function uno() {
-        let z: number;
+        EventListenerSortCards();
+        let numbercards: number;
         let input: string = prompt("Anzahl der Karten auswaehlen");
-        z = Number(input);
-
+        numbercards = Number(input);
 
         function placeDiv(_color: string, _typ: string, _y: number): void {
             let div: HTMLDivElement = document.createElement("div");
             document.body.appendChild(div);
             div.setAttribute("id", "a" + _y);
             document.getElementById("a" + _y).innerHTML += _typ;
-            HandCards.push("a"+_y);
             let s: CSSStyleDeclaration = div.style;
             s.backgroundColor = _color;
             s.left = (_y + 0.2) * 110 + "px";
@@ -509,20 +51,17 @@ namespace Aufgabe31 {
             }
             if (_color == "#0000ff") {
                 s.color = "white";
-
             }
         }
 
-        for (let i: number = 0; i < z; i++) {
-            let b: number = random(AllCards.length);
+        for (let i: number = 0; i < numbercards; i++) {
+            let b: number = CreateRandomNumber(AllCards.length);
             placeDiv(AllCards[b].color, AllCards[b].typ, i);
-            AllCards.splice(b,1);
+            let card: Card = AllCards.splice(b, 1)[0];
+            HandCards.push(card)
             continue;
         }
-         
-          console.log(HandCards);
+        console.log(HandCards);
         console.log(AllCards)
     }
-    
-    document.addEventListener("DOMContentLoaded", (uno))
 }
