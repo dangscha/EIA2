724dec8c1b72b88a1400a7758d737dc570eec0e7
Jan-Laptop <Jan-Laptop@DESKTOP-3BB5I02>
2018-11-10 19:11:36 +0100

    CodingStlye

--- Aufgabe 3/Aufgabe3.js
+++ Aufgabe 3/Aufgabe3.js
@@ -9,61 +9,74 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 var Aufgabe31;
 (function (Aufgabe31) {
     document.addEventListener("DOMContentLoaded", uno);
-    var AllCards = [{ color: "#0000ff", typ: "0" }, { color: "#0000ff", typ: "1" }, { color: "#0000ff", typ: "1" }, { color: "#0000ff", typ: "2" }, { color: "#0000ff", typ: "2" }, { color: "#0000ff", typ: "3" }, { color: "#0000ff", typ: "3" }, { color: "#0000ff", typ: "4" }, { color: "#0000ff", typ: "4" }, { color: "#0000ff", typ: "5" }, { color: "#0000ff", typ: "5" }, { color: "#0000ff", typ: "6" }, { color: "#0000ff", typ: "6" }, { color: "#0000ff", typ: "7" }, { color: "#0000ff", typ: "7" }, { color: "#0000ff", typ: "8" }, { color: "#0000ff", typ: "8" }, { color: "#0000ff", typ: "9" }, { color: "#0000ff", typ: "9" }, { color: "#0000ff", typ: "Reverse" }, { color: "#0000ff", typ: "Reverse" }, { color: "#0000ff", typ: "Plus 2" }, { color: "#0000ff", typ: "Plus 2" }, { color: "#0000ff", typ: "Skip" }, { color: "#0000ff", typ: "Skip" }, { color: "#ff0000", typ: "0" }, { color: "#ff0000", typ: "1" }, { color: "#ff0000", typ: "1" }, { color: "#ff0000", typ: "2" }, { color: "#ff0000", typ: "2" }, { color: "#ff0000", typ: "3" }, { color: "#ff0000", typ: "3" }, { color: "#ff0000", typ: "4" }, { color: "#ff0000", typ: "4" }, { color: "#ff0000", typ: "5" }, { color: "#ff0000", typ: "5" }, { color: "#ff0000", typ: "6" }, { color: "#ff0000", typ: "6" }, { color: "#ff0000", typ: "7" }, { color: "#ff0000", typ: "7" }, { color: "#ff0000", typ: "8" }, { color: "#ff0000", typ: "8" }, { color: "#ff0000", typ: "9" }, { color: "#ff0000", typ: "9" }, { color: "#ff0000", typ: "Reverse" }, { color: "#ff0000", typ: "Reverse" }, { color: "#ff0000", typ: "Plus 2" }, { color: "#ff0000", typ: "Plus 2" }, { color: "#ff0000", typ: "Skip" }, { color: "#ff0000", typ: "Skip" }, { color: "#ffff00", typ: "0" }, { color: "#ffff00", typ: "1" }, { color: "#ffff00", typ: "1" }, { color: "#ffff00", typ: "2" }, { color: "#ffff00", typ: "2" }, { color: "#ffff00", typ: "3" }, { color: "#ffff00", typ: "3" }, { color: "#ffff00", typ: "4" }, { color: "#ffff00", typ: "4" }, { color: "#ffff00", typ: "5" }, { color: "#ffff00", typ: "5" }, { color: "#ffff00", typ: "6" }, { color: "#ffff00", typ: "6" }, { color: "#ffff00", typ: "7" }, { color: "#ffff00", typ: "7" }, { color: "#ffff00", typ: "8" }, { color: "#ffff00", typ: "8" }, { color: "#ffff00", typ: "9" }, { color: "#ffff00", typ: "9" }, { color: "#ffff00", typ: "Reverse" }, { color: "#ffff00", typ: "Reverse" }, { color: "#ffff00", typ: "Plus 2" }, { color: "#ffff00", typ: "Plus 2" }, { color: "#ffff00", typ: "Skip" }, { color: "#ffff00", typ: "Skip" }, { color: "#00ff00", typ: "0" }, { color: "#00ff00", typ: "1" }, { color: "#00ff00", typ: "1" }, { color: "#00ff00", typ: "2" }, { color: "#00ff00", typ: "2" }, { color: "#00ff00", typ: "3" }, { color: "#00ff00", typ: "3" }, { color: "#00ff00", typ: "4" }, { color: "#00ff00", typ: "4" }, { color: "#00ff00", typ: "5" }, { color: "#00ff00", typ: "5" }, { color: "#00ff00", typ: "6" }, { color: "#00ff00", typ: "6" }, { color: "#00ff00", typ: "7" }, { color: "#00ff00", typ: "7" }, { color: "#00ff00", typ: "8" }, { color: "#00ff00", typ: "8" }, { color: "#00ff00", typ: "9" }, { color: "#00ff00", typ: "9" }, { color: "#00ff00", typ: "Reverse" }, { color: "#00ff00", typ: "Reverse" }, { color: "#00ff00", typ: "Plus 2" }, { color: "#00ff00", typ: "Plus 2" }, { color: "#00ff00", typ: "Skip" }, { color: "#00ff00", typ: "Skip" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Color" }, { color: "#000000", typ: "Color" }, { color: "#000000", typ: "Color" }, { color: "#000000", typ: "Color" },];
-    var HandCards = [];
+    var allCards = [{ color: "#0000ff", typ: "0" }, { color: "#0000ff", typ: "1" }, { color: "#0000ff", typ: "1" }, { color: "#0000ff", typ: "2" }, { color: "#0000ff", typ: "2" }, { color: "#0000ff", typ: "3" }, { color: "#0000ff", typ: "3" }, { color: "#0000ff", typ: "4" }, { color: "#0000ff", typ: "4" }, { color: "#0000ff", typ: "5" }, { color: "#0000ff", typ: "5" }, { color: "#0000ff", typ: "6" }, { color: "#0000ff", typ: "6" }, { color: "#0000ff", typ: "7" }, { color: "#0000ff", typ: "7" }, { color: "#0000ff", typ: "8" }, { color: "#0000ff", typ: "8" }, { color: "#0000ff", typ: "9" }, { color: "#0000ff", typ: "9" }, { color: "#0000ff", typ: "Reverse" }, { color: "#0000ff", typ: "Reverse" }, { color: "#0000ff", typ: "Plus 2" }, { color: "#0000ff", typ: "Plus 2" }, { color: "#0000ff", typ: "Skip" }, { color: "#0000ff", typ: "Skip" }, { color: "#ff0000", typ: "0" }, { color: "#ff0000", typ: "1" }, { color: "#ff0000", typ: "1" }, { color: "#ff0000", typ: "2" }, { color: "#ff0000", typ: "2" }, { color: "#ff0000", typ: "3" }, { color: "#ff0000", typ: "3" }, { color: "#ff0000", typ: "4" }, { color: "#ff0000", typ: "4" }, { color: "#ff0000", typ: "5" }, { color: "#ff0000", typ: "5" }, { color: "#ff0000", typ: "6" }, { color: "#ff0000", typ: "6" }, { color: "#ff0000", typ: "7" }, { color: "#ff0000", typ: "7" }, { color: "#ff0000", typ: "8" }, { color: "#ff0000", typ: "8" }, { color: "#ff0000", typ: "9" }, { color: "#ff0000", typ: "9" }, { color: "#ff0000", typ: "Reverse" }, { color: "#ff0000", typ: "Reverse" }, { color: "#ff0000", typ: "Plus 2" }, { color: "#ff0000", typ: "Plus 2" }, { color: "#ff0000", typ: "Skip" }, { color: "#ff0000", typ: "Skip" }, { color: "#ffff00", typ: "0" }, { color: "#ffff00", typ: "1" }, { color: "#ffff00", typ: "1" }, { color: "#ffff00", typ: "2" }, { color: "#ffff00", typ: "2" }, { color: "#ffff00", typ: "3" }, { color: "#ffff00", typ: "3" }, { color: "#ffff00", typ: "4" }, { color: "#ffff00", typ: "4" }, { color: "#ffff00", typ: "5" }, { color: "#ffff00", typ: "5" }, { color: "#ffff00", typ: "6" }, { color: "#ffff00", typ: "6" }, { color: "#ffff00", typ: "7" }, { color: "#ffff00", typ: "7" }, { color: "#ffff00", typ: "8" }, { color: "#ffff00", typ: "8" }, { color: "#ffff00", typ: "9" }, { color: "#ffff00", typ: "9" }, { color: "#ffff00", typ: "Reverse" }, { color: "#ffff00", typ: "Reverse" }, { color: "#ffff00", typ: "Plus 2" }, { color: "#ffff00", typ: "Plus 2" }, { color: "#ffff00", typ: "Skip" }, { color: "#ffff00", typ: "Skip" }, { color: "#00ff00", typ: "0" }, { color: "#00ff00", typ: "1" }, { color: "#00ff00", typ: "1" }, { color: "#00ff00", typ: "2" }, { color: "#00ff00", typ: "2" }, { color: "#00ff00", typ: "3" }, { color: "#00ff00", typ: "3" }, { color: "#00ff00", typ: "4" }, { color: "#00ff00", typ: "4" }, { color: "#00ff00", typ: "5" }, { color: "#00ff00", typ: "5" }, { color: "#00ff00", typ: "6" }, { color: "#00ff00", typ: "6" }, { color: "#00ff00", typ: "7" }, { color: "#00ff00", typ: "7" }, { color: "#00ff00", typ: "8" }, { color: "#00ff00", typ: "8" }, { color: "#00ff00", typ: "9" }, { color: "#00ff00", typ: "9" }, { color: "#00ff00", typ: "Reverse" }, { color: "#00ff00", typ: "Reverse" }, { color: "#00ff00", typ: "Plus 2" }, { color: "#00ff00", typ: "Plus 2" }, { color: "#00ff00", typ: "Skip" }, { color: "#00ff00", typ: "Skip" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Color" }, { color: "#000000", typ: "Color" }, { color: "#000000", typ: "Color" }, { color: "#000000", typ: "Color" },];
+    var handCards = [];
     var pileCards = [];
-    function CreateRandomNumber(x) {
-        return Math.floor(Math.random() * Math.floor(x));
-    }
     //Hauptfunktion
     function uno() {
         //Eventlistener
-        function EventListenerSortCards() {
+        eventListenerSortCards();
+        eventListenerAddCard();
+        eventListenerDiscard();
+        //Prompt
+        var numberCards;
+        var input = prompt("Anzahl der Karten auswaehlen");
+        numberCards = Number(input);
+        //Karten ausgeben
+        for (var i = 0; i < numberCards; i++) {
+            var randomNumber = createRandomNumber(allCards.length);
+            placeDiv(allCards[randomNumber].color, allCards[randomNumber].typ, i);
+            var card = allCards.splice(randomNumber, 1)[0];
+            handCards.push(card);
+            continue;
+        }
+        //Zuf�llige Nummer generieren
+        function createRandomNumber(x) {
+            return Math.floor(Math.random() * Math.floor(x));
+        }
+        function eventListenerSortCards() {
             var button = document.getElementById("button");
             button.addEventListener("click", sortCards);
         }
-        EventListenerSortCards();
-        function EventListenerAddCard() {
-            var drawcard = document.getElementById("Nachzieh");
-            drawcard.addEventListener("click", addCard);
+        function eventListenerAddCard() {
+            var drawCard = document.getElementById("Nachzieh");
+            drawCard.addEventListener("click", addCard);
         }
-        EventListenerAddCard();
-        function EventListenerDiscard() {
+        function eventListenerDiscard() {
             var discard = document.getElementById("content");
-            discard.addEventListener("click", RemoveCard);
+            discard.addEventListener("click", removeCard);
         }
-        EventListenerDiscard();
         //Ablegen
-        function RemoveCard() {
-            console.log(AllCards);
+        function removeCard() {
+            console.log(allCards);
         }
         //Sortieren
         function sortCards() {
-            console.log(HandCards);
-            console.log(AllCards);
-            HandCards.sort(function (card1, card2) {
+            console.log(handCards);
+            console.log(allCards);
+            handCards.sort(function (card1, card2) {
                 var textA = card1.color.toUpperCase();
                 var textB = card2.color.toUpperCase();
                 return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
             });
             deleteCards();
-            for (var i = 0; i < HandCards.length; i++) {
-                placeDiv(HandCards[i].color, HandCards[i].typ, i);
+            for (var i = 0; i < handCards.length; i++) {
+                placeDiv(handCards[i].color, handCards[i].typ, i);
             }
         }
         //add Card
         function addCard() {
-            console.log(HandCards);
-            console.log(AllCards);
+            console.log(handCards);
+            console.log(allCards);
             deleteCards();
             for (var i = 0; i < 1; i++) {
-                var randomnumber = CreateRandomNumber(AllCards.length);
-                var card = AllCards.splice(randomnumber, 1)[0];
-                HandCards.push(card);
+                var randomNumber = createRandomNumber(allCards.length);
+                var card = allCards.splice(randomNumber, 1)[0];
+                handCards.push(card);
             }
-            for (var i = 0; i < HandCards.length; i++) {
-                console.log(HandCards);
-                placeDiv(HandCards[i].color, HandCards[i].typ, i);
+            for (var i = 0; i < handCards.length; i++) {
+                console.log(handCards);
+                placeDiv(handCards[i].color, handCards[i].typ, i);
             }
         }
         //Delete Cards
@@ -76,10 +89,6 @@ var Aufgabe31;
             main.setAttribute("id", "content");
             document.getElementsByTagName("body")[0].appendChild(main);
         }
-        //Prompt
-        var numbercards;
-        var input = prompt("Anzahl der Karten auswaehlen");
-        numbercards = Number(input);
         //PlaceDiv
         function placeDiv(_color, _typ, _y) {
             var div = document.createElement("div");
@@ -96,16 +105,6 @@ var Aufgabe31;
                 s.color = "white";
             }
         }
-        //Karten ausgeben
-        for (var i = 0; i < numbercards; i++) {
-            var randomnumber = CreateRandomNumber(AllCards.length);
-            placeDiv(AllCards[randomnumber].color, AllCards[randomnumber].typ, i);
-            var card = AllCards.splice(randomnumber, 1)[0];
-            HandCards.push(card);
-            continue;
-        }
-        console.log(HandCards);
-        console.log(AllCards);
     }
 })(Aufgabe31 || (Aufgabe31 = {}));
 //# sourceMappingURL=Aufgabe3.js.map
--- Aufgabe 3/Aufgabe3.ts
+++ Aufgabe 3/Aufgabe3.ts
@@ -15,71 +15,84 @@ namespace Aufgabe31 {
         typ: string;
     }
 
-    let AllCards: Card[] = [{ color: "#0000ff", typ: "0" }, { color: "#0000ff", typ: "1" }, { color: "#0000ff", typ: "1" }, { color: "#0000ff", typ: "2" }, { color: "#0000ff", typ: "2" }, { color: "#0000ff", typ: "3" }, { color: "#0000ff", typ: "3" }, { color: "#0000ff", typ: "4" }, { color: "#0000ff", typ: "4" }, { color: "#0000ff", typ: "5" }, { color: "#0000ff", typ: "5" }, { color: "#0000ff", typ: "6" }, { color: "#0000ff", typ: "6" }, { color: "#0000ff", typ: "7" }, { color: "#0000ff", typ: "7" }, { color: "#0000ff", typ: "8" }, { color: "#0000ff", typ: "8" }, { color: "#0000ff", typ: "9" }, { color: "#0000ff", typ: "9" }, { color: "#0000ff", typ: "Reverse" }, { color: "#0000ff", typ: "Reverse" }, { color: "#0000ff", typ: "Plus 2" }, { color: "#0000ff", typ: "Plus 2" }, { color: "#0000ff", typ: "Skip" }, { color: "#0000ff", typ: "Skip" }, { color: "#ff0000", typ: "0" }, { color: "#ff0000", typ: "1" }, { color: "#ff0000", typ: "1" }, { color: "#ff0000", typ: "2" }, { color: "#ff0000", typ: "2" }, { color: "#ff0000", typ: "3" }, { color: "#ff0000", typ: "3" }, { color: "#ff0000", typ: "4" }, { color: "#ff0000", typ: "4" }, { color: "#ff0000", typ: "5" }, { color: "#ff0000", typ: "5" }, { color: "#ff0000", typ: "6" }, { color: "#ff0000", typ: "6" }, { color: "#ff0000", typ: "7" }, { color: "#ff0000", typ: "7" }, { color: "#ff0000", typ: "8" }, { color: "#ff0000", typ: "8" }, { color: "#ff0000", typ: "9" }, { color: "#ff0000", typ: "9" }, { color: "#ff0000", typ: "Reverse" }, { color: "#ff0000", typ: "Reverse" }, { color: "#ff0000", typ: "Plus 2" }, { color: "#ff0000", typ: "Plus 2" }, { color: "#ff0000", typ: "Skip" }, { color: "#ff0000", typ: "Skip" }, { color: "#ffff00", typ: "0" }, { color: "#ffff00", typ: "1" }, { color: "#ffff00", typ: "1" }, { color: "#ffff00", typ: "2" }, { color: "#ffff00", typ: "2" }, { color: "#ffff00", typ: "3" }, { color: "#ffff00", typ: "3" }, { color: "#ffff00", typ: "4" }, { color: "#ffff00", typ: "4" }, { color: "#ffff00", typ: "5" }, { color: "#ffff00", typ: "5" }, { color: "#ffff00", typ: "6" }, { color: "#ffff00", typ: "6" }, { color: "#ffff00", typ: "7" }, { color: "#ffff00", typ: "7" }, { color: "#ffff00", typ: "8" }, { color: "#ffff00", typ: "8" }, { color: "#ffff00", typ: "9" }, { color: "#ffff00", typ: "9" }, { color: "#ffff00", typ: "Reverse" }, { color: "#ffff00", typ: "Reverse" }, { color: "#ffff00", typ: "Plus 2" }, { color: "#ffff00", typ: "Plus 2" }, { color: "#ffff00", typ: "Skip" }, { color: "#ffff00", typ: "Skip" }, { color: "#00ff00", typ: "0" }, { color: "#00ff00", typ: "1" }, { color: "#00ff00", typ: "1" }, { color: "#00ff00", typ: "2" }, { color: "#00ff00", typ: "2" }, { color: "#00ff00", typ: "3" }, { color: "#00ff00", typ: "3" }, { color: "#00ff00", typ: "4" }, { color: "#00ff00", typ: "4" }, { color: "#00ff00", typ: "5" }, { color: "#00ff00", typ: "5" }, { color: "#00ff00", typ: "6" }, { color: "#00ff00", typ: "6" }, { color: "#00ff00", typ: "7" }, { color: "#00ff00", typ: "7" }, { color: "#00ff00", typ: "8" }, { color: "#00ff00", typ: "8" }, { color: "#00ff00", typ: "9" }, { color: "#00ff00", typ: "9" }, { color: "#00ff00", typ: "Reverse" }, { color: "#00ff00", typ: "Reverse" }, { color: "#00ff00", typ: "Plus 2" }, { color: "#00ff00", typ: "Plus 2" }, { color: "#00ff00", typ: "Skip" }, { color: "#00ff00", typ: "Skip" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Color" }, { color: "#000000", typ: "Color" }, { color: "#000000", typ: "Color" }, { color: "#000000", typ: "Color" },]
-    let HandCards: Card[] = [];
+    let allCards: Card[] = [{ color: "#0000ff", typ: "0" }, { color: "#0000ff", typ: "1" }, { color: "#0000ff", typ: "1" }, { color: "#0000ff", typ: "2" }, { color: "#0000ff", typ: "2" }, { color: "#0000ff", typ: "3" }, { color: "#0000ff", typ: "3" }, { color: "#0000ff", typ: "4" }, { color: "#0000ff", typ: "4" }, { color: "#0000ff", typ: "5" }, { color: "#0000ff", typ: "5" }, { color: "#0000ff", typ: "6" }, { color: "#0000ff", typ: "6" }, { color: "#0000ff", typ: "7" }, { color: "#0000ff", typ: "7" }, { color: "#0000ff", typ: "8" }, { color: "#0000ff", typ: "8" }, { color: "#0000ff", typ: "9" }, { color: "#0000ff", typ: "9" }, { color: "#0000ff", typ: "Reverse" }, { color: "#0000ff", typ: "Reverse" }, { color: "#0000ff", typ: "Plus 2" }, { color: "#0000ff", typ: "Plus 2" }, { color: "#0000ff", typ: "Skip" }, { color: "#0000ff", typ: "Skip" }, { color: "#ff0000", typ: "0" }, { color: "#ff0000", typ: "1" }, { color: "#ff0000", typ: "1" }, { color: "#ff0000", typ: "2" }, { color: "#ff0000", typ: "2" }, { color: "#ff0000", typ: "3" }, { color: "#ff0000", typ: "3" }, { color: "#ff0000", typ: "4" }, { color: "#ff0000", typ: "4" }, { color: "#ff0000", typ: "5" }, { color: "#ff0000", typ: "5" }, { color: "#ff0000", typ: "6" }, { color: "#ff0000", typ: "6" }, { color: "#ff0000", typ: "7" }, { color: "#ff0000", typ: "7" }, { color: "#ff0000", typ: "8" }, { color: "#ff0000", typ: "8" }, { color: "#ff0000", typ: "9" }, { color: "#ff0000", typ: "9" }, { color: "#ff0000", typ: "Reverse" }, { color: "#ff0000", typ: "Reverse" }, { color: "#ff0000", typ: "Plus 2" }, { color: "#ff0000", typ: "Plus 2" }, { color: "#ff0000", typ: "Skip" }, { color: "#ff0000", typ: "Skip" }, { color: "#ffff00", typ: "0" }, { color: "#ffff00", typ: "1" }, { color: "#ffff00", typ: "1" }, { color: "#ffff00", typ: "2" }, { color: "#ffff00", typ: "2" }, { color: "#ffff00", typ: "3" }, { color: "#ffff00", typ: "3" }, { color: "#ffff00", typ: "4" }, { color: "#ffff00", typ: "4" }, { color: "#ffff00", typ: "5" }, { color: "#ffff00", typ: "5" }, { color: "#ffff00", typ: "6" }, { color: "#ffff00", typ: "6" }, { color: "#ffff00", typ: "7" }, { color: "#ffff00", typ: "7" }, { color: "#ffff00", typ: "8" }, { color: "#ffff00", typ: "8" }, { color: "#ffff00", typ: "9" }, { color: "#ffff00", typ: "9" }, { color: "#ffff00", typ: "Reverse" }, { color: "#ffff00", typ: "Reverse" }, { color: "#ffff00", typ: "Plus 2" }, { color: "#ffff00", typ: "Plus 2" }, { color: "#ffff00", typ: "Skip" }, { color: "#ffff00", typ: "Skip" }, { color: "#00ff00", typ: "0" }, { color: "#00ff00", typ: "1" }, { color: "#00ff00", typ: "1" }, { color: "#00ff00", typ: "2" }, { color: "#00ff00", typ: "2" }, { color: "#00ff00", typ: "3" }, { color: "#00ff00", typ: "3" }, { color: "#00ff00", typ: "4" }, { color: "#00ff00", typ: "4" }, { color: "#00ff00", typ: "5" }, { color: "#00ff00", typ: "5" }, { color: "#00ff00", typ: "6" }, { color: "#00ff00", typ: "6" }, { color: "#00ff00", typ: "7" }, { color: "#00ff00", typ: "7" }, { color: "#00ff00", typ: "8" }, { color: "#00ff00", typ: "8" }, { color: "#00ff00", typ: "9" }, { color: "#00ff00", typ: "9" }, { color: "#00ff00", typ: "Reverse" }, { color: "#00ff00", typ: "Reverse" }, { color: "#00ff00", typ: "Plus 2" }, { color: "#00ff00", typ: "Plus 2" }, { color: "#00ff00", typ: "Skip" }, { color: "#00ff00", typ: "Skip" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Plus 4" }, { color: "#000000", typ: "Color" }, { color: "#000000", typ: "Color" }, { color: "#000000", typ: "Color" }, { color: "#000000", typ: "Color" },]
+    let handCards: Card[] = [];
     let pileCards: Card[] = [];
 
-    function CreateRandomNumber(x: number) {
-        return Math.floor(Math.random() * Math.floor(x))
-    }
     //Hauptfunktion
     function uno() {
-        //Eventlistener
 
-        function EventListenerSortCards() {
+        //Eventlistener
+        eventListenerSortCards();
+        eventListenerAddCard();
+        eventListenerDiscard();
+
+        //Prompt
+        let numberCards: number;
+        let input: string = prompt("Anzahl der Karten auswaehlen");
+        numberCards = Number(input);
+
+        //Karten ausgeben
+        for (let i: number = 0; i < numberCards; i++) {
+            let randomNumber: number = createRandomNumber(allCards.length);
+            placeDiv(allCards[randomNumber].color, allCards[randomNumber].typ, i);
+            let card: Card = allCards.splice(randomNumber, 1)[0];
+            handCards.push(card)
+            continue;
+        }
+
+        //Zufällige Nummer generieren
+        function createRandomNumber(x: number) {
+            return Math.floor(Math.random() * Math.floor(x))
+        }
+
+        function eventListenerSortCards() {
             let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
             button.addEventListener("click", sortCards)
         }
-        EventListenerSortCards();
 
-        function EventListenerAddCard() {
-            let drawcard: HTMLDivElement = <HTMLDivElement>document.getElementById("Nachzieh");
-            drawcard.addEventListener("click", addCard);
+        function eventListenerAddCard() {
+            let drawCard: HTMLDivElement = <HTMLDivElement>document.getElementById("Nachzieh");
+            drawCard.addEventListener("click", addCard);
         }
-        EventListenerAddCard();
-        
-        function EventListenerDiscard() {
-            let discard: HTMLDivElement = <HTMLDivElement>document.getElementById("content");
-            discard.addEventListener("click", RemoveCard);}
-        EventListenerDiscard();
-         
-        //Ablegen
-        function RemoveCard(){
-            
-                        console.log(AllCards);
 
-            }
+        function eventListenerDiscard() {
+            let discard: HTMLDivElement = <HTMLDivElement>document.getElementById("content");
+            discard.addEventListener("click", removeCard);
+        }
+
+        //Ablegen
+        function removeCard() {
+            console.log(allCards);
+        }
         //Sortieren
         function sortCards() {
-            console.log(HandCards);
-            console.log(AllCards);
-            HandCards.sort(function(card1, card2) {
+            console.log(handCards);
+            console.log(allCards);
+            handCards.sort(function(card1, card2) {
                 var textA = card1.color.toUpperCase();
                 var textB = card2.color.toUpperCase();
                 return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
             });
             deleteCards();
-
-            for (let i: number = 0; i < HandCards.length; i++) {
-
-                placeDiv(HandCards[i].color, HandCards[i].typ, i)
+            for (let i: number = 0; i < handCards.length; i++) {
+                placeDiv(handCards[i].color, handCards[i].typ, i)
             }
         }
-        
+
         //add Card
         function addCard() {
-            console.log(HandCards);
-            console.log(AllCards);
+            console.log(handCards);
+            console.log(allCards);
             deleteCards();
             for (let i: number = 0; i < 1; i++) {
-                let randomnumber: number = CreateRandomNumber(AllCards.length);
-                let card: Card = AllCards.splice(randomnumber, 1)[0];
-                HandCards.push(card)
+                let randomNumber: number = createRandomNumber(allCards.length);
+                let card: Card = allCards.splice(randomNumber, 1)[0];
+                handCards.push(card)
             }
-            for (let i: number = 0; i < HandCards.length; i++) {
-                console.log(HandCards);
-                placeDiv(HandCards[i].color, HandCards[i].typ, i);
-
+            for (let i: number = 0; i < handCards.length; i++) {
+                console.log(handCards);
+                placeDiv(handCards[i].color, handCards[i].typ, i);
             }
         }
 
@@ -89,17 +102,10 @@ namespace Aufgabe31 {
             if (node.parentNode) {
                 node.parentNode.removeChild(node);
             }
-
             let main: HTMLElement = document.createElement("main");
             main.setAttribute("id", "content");
             document.getElementsByTagName("body")[0].appendChild(main);
         }
-
-        //Prompt
-        let numbercards: number;
-        let input: string = prompt("Anzahl der Karten auswaehlen");
-        numbercards = Number(input);
-
 
         //PlaceDiv
         function placeDiv(_color: string, _typ: string, _y: number) {
@@ -117,15 +123,5 @@ namespace Aufgabe31 {
                 s.color = "white";
             }
         }
-        //Karten ausgeben
-        for (let i: number = 0; i < numbercards; i++) {
-            let randomnumber: number = CreateRandomNumber(AllCards.length);
-            placeDiv(AllCards[randomnumber].color, AllCards[randomnumber].typ, i);
-            let card: Card = AllCards.splice(randomnumber, 1)[0];
-            HandCards.push(card)
-            continue;
-        }
-        console.log(HandCards);
-        console.log(AllCards)
     }
 }
