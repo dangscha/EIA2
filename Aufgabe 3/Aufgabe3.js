e75fb69ec82f886c8ea3a71abc61237ff2625100
Jan-Laptop <Jan-Laptop@DESKTOP-3BB5I02>
2018-11-10 19:12:01 +0100

    a

--- Aufgabe 3/Aufgabe3.js
+++ Aufgabe 3/Aufgabe3.js
@@ -1,8 +1,8 @@
 /**
-Aufgabe: 2b
+Aufgabe: 3
 Name: Jan Dangschat
 Matrikel: 259522
-Datum: 28.10.2018
+Datum: 10.11.2018
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 **/
